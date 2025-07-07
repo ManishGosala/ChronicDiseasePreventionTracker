d5940137d860d4e79f2a8d58a7118fbae255c638
Dhanasri101 <153172436+Dhanasri101@users.noreply.github.com>
2025-07-07 11:28:46 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,15 @@
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import joblib
+
+# Sample data
+data = pd.read_csv({
+    "C:\Users\dhanasri\Downloads\health_metrics_50.csv"
+})
+
+X = data[['glucose', 'bp', 'bmi', 'age']]
+y = data['risk']
+
+model = RandomForestClassifier()
+model.fit(X, y)
+joblib.dump(model, 'chronic_model.pkl')
